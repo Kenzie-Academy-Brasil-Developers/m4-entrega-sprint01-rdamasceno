67cd36c7d0775b78c2ffbf5d1fabe11e3f4e9ed3
raphael damasceno <rdamasceno185@gmail.com>
2022-11-26 18:46:20 -0300

    project2-done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+const app = express();
+const port = 3001;
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-const app = express()
+app.listen(port, () => {
+  console.log(`The app is running in port ${port}`);
+});
 
-
-export default app
+export default app;
